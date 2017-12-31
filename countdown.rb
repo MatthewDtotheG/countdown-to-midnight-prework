7f55e454d705c72d7ec884c2a727d63fcac5d41f
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-31 19:06:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown(number)
-  while seconds_to_midnight > 0
+  while number > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
   end
